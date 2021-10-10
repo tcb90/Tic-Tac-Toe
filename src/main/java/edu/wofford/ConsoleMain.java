b40213e079bc7ce4c35780189848eede335cc217
tcb90 <52583263+tcb90@users.noreply.github.com>
2021-10-10 12:20:58 -0400

    board set up

--- src/main/java/edu/wofford/ConsoleMain.java
+++ src/main/java/edu/wofford/ConsoleMain.java
@@ -1,11 +1,14 @@
 package edu.wofford;
-
 import java.util.Scanner;
 
 public class ConsoleMain {
-
-	public static void main(String[] args) {
-		Scanner keyboard = new Scanner(System.in);
-
-	}
+    String[][] spots = {{" ", " ", " "}, {" ", " ", " "}, {" ", " ", " "}};
+    private String board = spots[0][0] + "|" + spots[0][1] + "|" + spots[0][2]
+            + "\n" + "-----" + "\n" + spots[1][0] + "|" + spots[1][1]
+            + "|" + spots[1][2] + "\n" + "-----" + "\n" + spots[2][0] + "|"
+            + spots[2][1] + "|" + spots[2][2];
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        
+    }
 }
